3f4987432984cc244976df6e02e9b64cc97a8c0a
george.dev <gsoriano.ext@gmail.com>
2024-06-20 16:18:41 +0800

    update workexperience

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Footer from "../components/Footer";
 import Hero from "../components/Hero";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import WorkExperience from "../components/WorkExperience";
+// import WorkExperience from "../components/WorkExperience";
 
 export default function Page() {
   return (
@@ -12,7 +12,7 @@ export default function Page() {
       <div className="mt-20 space-y-32">
         <Projects />
         <Skills />
-        <WorkExperience />
+        {/* <WorkExperience /> */}
       </div>
       <CTA />
     </div>
